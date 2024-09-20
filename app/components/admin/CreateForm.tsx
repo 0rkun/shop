e095c436136fe12b0addf14269bc85b2ac40761c
Orkun Yılmaz <106023667+0rkun@users.noreply.github.com>
2024-09-20 23:19:37 +0300

    Update CreateForm.tsx

--- app/components/admin/CreateForm.tsx
+++ app/components/admin/CreateForm.tsx
@@ -72,59 +72,13 @@ const CreateForm = () => {
   });
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-    console.log("data", data);
-
-    let uploadedImg;
-
     const handleChange = async () => {
       toast.success("Yükleme işlemi basarılı !!!");
-      try {
-        const storage = getStorage(firebaseApp);
-        const storageRef = ref(storage, "images/shop.jpg");
 
-        const uploadTask = uploadBytesResumable(storageRef, img);
-        await new Promise<void>((resolve, reject) => {
-          uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-              // Observe state change events such as progress, pause, and resume
-              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-              const progress =
-                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-              console.log("Upload is " + progress + "% done");
-              switch (snapshot.state) {
-                case "paused":
-                  console.log("Upload is paused");
-                  break;
-                case "running":
-                  console.log("Upload is running");
-                  break;
-              }
-            },
-            (error) => {
-              reject(error);
-            },
-            () => {
-              getDownloadURL(uploadTask.snapshot.ref)
-                .then((downloadURL) => {
-                  console.log("File available at", downloadURL);
-                  uploadedImg = downloadURL;
-                  resolve();
-                })
-                .catch((error) => {
-                  console.log(error);
-                });
-            }
-          );
-        });
-      } catch (error) {
-        console.log(error);
-      }
     };
     await handleChange();
 
-    let newData = { ...data, image: uploadedImg };
-
+    let newData = { ...data };
     axios
       .post("/api/product", newData)
       .then(() => {
